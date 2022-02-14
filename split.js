f1917f988fb6b07281a16dd8a375886f68596114
Md-Omar-Shahariar <osafridi00@gmail.com>
2022-02-14 18:31:12 +0600

    substr

--- split.js
+++ split.js
@@ -1,6 +1,11 @@
+//split
 let string = 'amar sonar bangla ami tomay valobashi'
 let str = string.split(' ')
 console.log(str);
-
+//slice
 let slice = string.slice(0,4)
 console.log(slice);
+
+//substr
+let substr1 = string.substr(5,5)
+console.log(substr1);
