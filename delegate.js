7b22f6c5b4719a026bef6ffb60735eb62872f69b
Md-Omar-Shahariar <osafridi00@gmail.com>
2022-02-16 00:21:06 +0600

    remove Item

--- delegate.js
+++ delegate.js
@@ -1,16 +1,45 @@
-document.getElementById('remove-item').addEventListener('click',function(){
-    // const parent = document.getElementById('item-container')
-    // parent.removeChild(parent.lastChild)
-    // const childNode = document.getElementsByClassName('list-item')
-    const parent = document.getElementById('item-container')
-    // parent.removeChild(parent.lastChild)
-    function remove(){
-       document.getElementById('item-container').removeChild(parent.lastChild)
-    }
-    remove()
+// document.getElementById('remove-item').addEventListener('click',function(){
+//     // const parent = document.getElementById('item-container')
+//     // parent.removeChild(parent.lastChild)
     
-    console.log('buttonPressed');
-})
+//     // const parent = document.getElementById('item-container')
+//     // parent.removeChild(parent.lastChild)
+//     const list  = document.getElementsByClassName('list-item')
+//     // this.removeChild(list.)
+//     for(item of list){
+//         if(item.innerText.includes(list.length)){
+            
+//         }
+//     }
+//     console.log(list.length);
+    
+//     console.log('buttonPressed');
+// })
+function removeChildLast(){
+    const parent = document.getElementById('item-container')
+    console.log(parent);
+    //console.log(parent.removeChild(parent.lastChild)); catch whitespace along element 
+
+    console.log(parent.removeChild(parent.lastElementChild));
+    console.log('button Pressed');
+    // let list = document.getElementsByClassName('item-container')
+    // list.removeChild(list.childNodes[0]);
+
+    
+    // for(item of list){
+    //             if(item.innerText.includes(list.length)){
+    //                 list.
+    //                 console.log(item);
+    //             }
+    //         }
+
+
+    /* Another solution */
+    // let list  = document.getElementsByClassName('list-item')
+    // console.log(list.length);
+    // list[list.length-1].remove()
+
+}
 document.getElementById('add-item').addEventListener('click',function(){
     const childNode = document.getElementsByClassName('list-item')
     const li = document.createElement('li')
